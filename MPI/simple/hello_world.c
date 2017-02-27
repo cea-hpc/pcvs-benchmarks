7d1c9377056f2f83dc8a5b0e350dd9a95c3d2090
Julien Adam <adamj@paratools.com>
2017-02-27 15:01:53 +0100

    UnitTests cleanups

--- MPI/simple/hello_world.c
+++ MPI/simple/hello_world.c
@@ -1,23 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
-/*#ifdef MPC_MODULE_MPC_MPI*/
 #include <mpi.h>
-/*#else*/
-/*#include <mpc.h>*/
-/*#endif*/
 
 int main(int argc,char * argv[])
 {
-/*#ifdef MPC_MODULE_MPC_MPI*/
-
 	int rank, size;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	printf("Nb tasks: %d, Hello world from rank %d!!!\n", size, rank);
 	MPI_Finalize();
-/*#else*/
 	printf("Hello world\n");
-/*#endif*/
 	return EXIT_SUCCESS;
 }
