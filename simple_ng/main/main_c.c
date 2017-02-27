15e24ee9f3a3bcede3e3e0aa0a4e8afeb127f052
Julien Adam <adamj@paratools.com>
2017-02-27 12:14:56 +0100

    Cleanup MPI directory

--- simple_ng/main/main_c.c
+++
@@ -1,5 +0,0 @@
-#include <mpc.h>
-
-int main(int argc, char ** argv){
-	return 0;
-}
--- simple_ng/main/mpc_status.c
+++
@@ -1,8 +0,0 @@
-#include <mpc.h>
-#include <stdio.h>
-
-int main(int argc, char** argv){
-  MPC_Config_Status_Print(stderr);
-
-  return 0;
-}
