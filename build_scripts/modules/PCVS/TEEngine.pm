ae9a2c7e74c1542b25d8863ad64c2e79d8997ba5
Julien Adam <adamj@paratools.com>
2017-03-16 17:08:25 +0100

    First unfolding system configuration from environment/

--- build_scripts/modules/PCVS/TEEngine.pm
+++ build_scripts/modules/PCVS/TEEngine.pm
@@ -5,17 +5,95 @@ use Exporter;
 use Data::Dumper;
 use XML::Writer;
 use YAML qw(LoadFile DumpFile); 
+$YAML::numify = 1;
 use vars qw(@ISA @EXPORT @EXPORT_OK);
 
 @ISA = 'Exporter';
 @EXPORT = qw(engine_init engine_fini engine_unfold_file);
 @EXPORT_OK = qw();
 
-my $myconf;
+my $sysconf;
+my %testconf;
+
+sub get_if_exists
+{
+	my ($p) = @_;
+
+	if(exists $sysconf->{'tests'}{$p})
+	{
+		return $sysconf->{'tests'}{$p};
+	}
+	else
+	{
+		return undef;
+	}
+}
 
 sub engine_init
 {
-	($myconf) = @_;
+	($sysconf) = @_;
+
+	my @system_fields = ("n_node", "n_proc", "n_mpi", "n_omp", "n_core", "net", "sched");
+
+	foreach my $field(@system_fields)
+	{
+		$testconf{$field} = get_if_exists($field);
+	
+		if(defined $testconf{$field})
+		{
+			$testconf{$field} = undef if(defined $testconf{$field} and scalar $testconf{$field} eq 0);
+			my @list_values;
+			foreach my $el(@{ $testconf{$field} } )
+			{
+				if($el =~ /^([0-9]+):([0-9]+):([+*^])?([0-9]+)*$/)
+				{
+					my ($min, $max, $op, $step, $prev);
+					$min = $1;
+					$max  = $2;
+					$op = (defined $3) ? $3 : "+";
+					$step = (defined $4) ? $4 : "1";
+				
+					if($op eq "^")
+					{
+						$min = int($min ** (1/$step));
+						$prev = $min;
+					}
+
+					while($min <= $max)
+					{
+						push @list_values, $min;
+					} 
+					continue 
+					{
+						#switch-case does not natively exist w/ Perl
+       						if   ($op eq "+") {$min+=$step;}
+       						elsif($op eq "*") {$min*=$step;}
+       						elsif($op eq "^") {$prev+=1; $min=$prev**$step;}
+       						else {die("$field: Unknown operator '$_'");}
+					}
+				}
+				elsif($el =~ /^([0-9]+)(-([0-9]+))*$/)
+				{
+					my ($min, $max);
+					$min = $1; $max = (defined $2) ? $3 : $min;
+					while($min <= $max)
+					{
+						push @list_values, $min;
+					} continue {$min++;}
+				}
+				elsif($field =~ /^n_/) #iterators prefixed with 'n_' should fall into previous conditinos
+				{
+					die("$field only takes numeric, 'a:b:c' or 'a-b' interval values !!");
+				}
+				else
+				{
+					push @list_values, $el;
+				}
+			}
+			$testconf{$field} = \@list_values;
+		}
+	}
+	print Dumper(\%testconf);
 }
 
 sub engine_unfold_test
