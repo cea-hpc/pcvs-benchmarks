98fb93255ba20f67ff57ff698dca33684d4b5e0f
Julien Adam <adamj@paratools.com>
2017-04-03 12:19:35 +0200

    DOC: Helper

--- build_scripts/modules/PCVS/Helper.pm
+++ build_scripts/modules/PCVS/Helper.pm
@@ -13,15 +13,31 @@ our @EXPORT_OK = qw();
 our $conf;
 our ($buildir, $internaldir, $srcdir);
 
+
+###########################################################################
+# Helper init: register the configuration for using it later
+# Args:
+#  - $conf: a reference to system %gconf
 sub helper_init
 {
 	($conf) = @_;
+
+	#aliases to be faster
 	$buildir = $conf->{'build'};
 	$srcdir = $conf->{src};
 	$internaldir = "$srcdir/build_scripts";
 	
 }
 
+
+###########################################################################
+# Convert a path to an absolute path with the given prefix
+# Args:
+#   - $path : path to check
+#   - $prefix: path to preprend
+#
+# Returns:
+#  The string, modified or not
 sub helper_convert_absolute
 {
 	my($path, $prefix) = @_;
@@ -35,11 +51,22 @@ sub helper_convert_absolute
 	return $path;
 }
 
+###########################################################################
+# Convert time in seconds in an array follwing DHMS format
+# Args
+#  - $t: time, in seconds
+#
+# Returns 4-value array: (DD, HH, MM, SS)
 sub helper_convert_time {
 	my $t = shift;
 	return int($t / 86400), (gmtime($t))[2, 1, 0];
 }
 
+###########################################################################
+# Create and clean paths
+# Args:
+#   - $path : path to create if not exist
+#   - $with_clean : has the path to be cleaned before returning from the function ?
 sub helper_clean_path
 {
 	my ($path,$with_clean) = @_;
@@ -47,24 +74,54 @@ sub helper_clean_path
 	(pathempty($path) or die ("pathempty(): $!")) if ($with_clean); 
 }
 
+###########################################################################
+# Prefix the value with a pattern if the value is defined (shortcut)
+# Args:
+#   - $value: the value to check
+#   - $prefix : prefix to prepend
+#
+# Returns:
+#   - The value, prepended or not
 sub helper_prefix_if_exists
 {
 	my ($prefix, $value) = @_;
 	return ($value  ? $prefix.$value : "");
 }
 
+###########################################################################
+# Round the value to the last integer (avoiding to load Math module)
+# Args:
+#  - $val: the value to round
+#
+# Returns:
+# Rounded value
 sub helper_floor
 {
 	my ($val) = @_;
 	return ($val == int($val)) ? $val : int($val);
 }
 
+###########################################################################
+# Round the value to the next integer (avoiding to load Math module)
+# Args:
+#  - $val: the value to round
+#
+# Returns:
+# Rounded value
 sub helper_ceil
 {
 	my ($val) = @_;
 	return ($val == int($val)) ? $val : int($val)+1;
 }
 
+###########################################################################
+# List content of directory
+# Args:
+#  - $filepath: directory to look for
+#  - $ext: the extension to remove from list entries
+#
+# Returns:
+# List of files found in directory
 sub  helper_lister
 {
 	my ($filepath, $ext) = @_;
@@ -72,12 +129,22 @@ sub  helper_lister
 	return grep(s/\.$ext$//, readdir($dirlist));
 }
 
+###########################################################################
+# List main directories used for PCVS validation. We think it is a bad idea
+# to have the hard-written list of directories here. This should be replaced
+# Args: No Args
+#
+# Returns:
+# Array of directories matching hard-written pattern
 sub  helper_list_avail_dirs
 {
 	opendir(my $dirlist, "$srcdir");
 	return grep { -d "$srcdir/$_" and /^(applications|MPI|OpenMP|performance|reproducers|Threads|Hybrid)/} readdir($dirlist);
 }
 
+###########################################################################
+# Check if user set an option not requiring to run the validation
+# Args: No Args
 sub helper_do_not_run_validation
 {
 	my $ret = 0;
@@ -120,6 +187,15 @@ sub helper_do_not_run_validation
 	return $ret;
 }
 
+###########################################################################
+# Detect compiler, depending on file extension from parameter list.
+# Sadly, switch-case construct does not exist in Perl core and we want to restrict
+# number of embedded modules.
+# Args:
+#  - @list_files: list of source files (only the first one is used)
+#
+# Returns:
+# A string matching to a defined 'compiler' field: c, cxx, f77, ...
 sub helper_detect_compiler
 {
 	my @list_files = @_;
