ba18048ff733e1e8ac266da833ddd51455344561
Julien Adam <adamj@paratools.com>
2018-09-25 11:34:18 +0200

    upgrade script fixes

--- compilers/gcc/tools/gcc-valscript.pl
+++ compilers/gcc/tools/gcc-valscript.pl
@@ -3,6 +3,59 @@
 use strict;
 use warnings;
 
-print "Validation Script not written yet !\n";
-exit 42;
+use JSON;
+use Data::Dumper;
 
+my $ret = 42;
+my $out = "";
+(print "Missing arg" and exit $ret) if(scalar(@ARGV) le 0);
+my $arg = decode_json($ARGV[0]);
+
+# set regex depending on the condition
+my @regexes;
+push (@regexes, @{ %{ $arg }{'regex-err'}  } ) if (defined %{$arg}{'regex-err'});
+push (@regexes, @{ %{ $arg }{'regex-warn'} } ) if (defined %{$arg}{'regex-warn'});
+my $nb_success = 0;
+my @matches = ();
+
+(print "No regex to filter with !" and exit 0) if(scalar(@regexes) eq 0);
+while(<STDIN>)
+{
+	foreach my $reg (@regexes )
+	{
+		if(grep { /$reg/ } $_)
+		{
+			push @matches, $reg;
+			$nb_success+=1;
+		}
+	}
+
+	$out .= $_;
+}
+
+my $diff = scalar(@regexes) - $nb_success;
+print scalar(@regexes)."- $nb_success = $diff\n";
+if($diff gt 0)
+{
+	print "FAIL !!! $diff regex(es) did not match: \n";
+	foreach my $r (@regexes)
+	{
+		if(not grep (/$r/, @matches))
+		{
+			print "\t - $r\n";
+		}
+	}
+
+	print "ACTUAL OUTPUT FOR DEBUG==========\n";
+	print "$out";
+	print "\n=================================\n";
+	$ret = 1;
+}
+else
+{
+	print "SUCCESS !!! All regexes found\n";
+	$ret = 0;
+}
+
+
+exit $ret;
