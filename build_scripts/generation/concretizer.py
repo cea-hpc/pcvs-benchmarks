eff2216b57aeb99de87af2c6cf6876b15d7642e2
Julien Adam <adamj@paratools.com>
2019-03-21 11:27:55 +0100

    Add forgotten concretize script

---
+++ build_scripts/generation/concretizer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import sys
+import spack.spec as s
+import spack.modules.tcl as t
+for package in sys.argv[1:]:
+tmp = s.Spec(package)
+tmp.concretize()
+print ((t.TclModulefileWriter(tmp)).layout.use_name)
+print tmp.format("${prefix}")
