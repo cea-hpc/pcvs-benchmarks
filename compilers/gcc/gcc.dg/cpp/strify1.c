410d4fc80d21a11b4b8f05cc06e6f9e3a8a783a4
Julien Adam <adamj@paratools.com>
2020-10-23 08:51:23 +0200

    GCC: upgrade compilers/gcc relatively to pcvs-rt

--- compilers/gcc/gcc.dg/cpp/strify1.c
+++ compilers/gcc/gcc.dg/cpp/strify1.c
@@ -8,3 +8,4 @@
 #define OK2(x) x#x	/* No problem.  */
 #define bad1(x) #	/* { dg-error "followed by a macro parameter" "#1" } */
 #define bad2(x) #y	/* { dg-error "followed by a macro parameter" "#2" } */
+
