bee123aa9f4670535bfc73a878f5700b6ece5f84
Julien Adam <adamj@paratools.com>
2021-08-03 14:16:31 +0200

    Remove unecessary code + add pcvs-core submodule

--- build_scripts/generation/jchronoss/tests/integration/wrapper.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "STARTED BY WRAPPER $@"
-"$@" || exit 1
--- build_scripts/generation/jchronoss/tools/resources/update_licence_header.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-for cur in `ls *.cpp && ls *.h`
-do
-echo "Doing $cur"
-FILE=$cur 
-cat ../tools/resources/header_cpp > $FILE.new && tail -n `echo "\`cat $FILE | wc -l\` - 24" | bc` $FILE >> $FILE.new
-#mv $FILE.new $FILE
-done
