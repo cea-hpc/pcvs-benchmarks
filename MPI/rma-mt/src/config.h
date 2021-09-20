353236b7704e35518c97de865e9d9403b38807b1
Brelle Emmanuel <emmanuel.brelle@atos.net>
2021-09-20 11:25:18 +0200

    Added generated files for MPI/rma-mt test suite

--- MPI/rma-mt/src/config.h
+++ MPI/rma-mt/src/config.h
@@ -55,12 +55,8 @@
 /* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
 
-/* Define to the sub-directory in which libtool stores uninstalled libraries.
-   */
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
 #define LT_OBJDIR ".libs/"
-
-/* Define to 1 if your C compiler doesn't accept -c and -o together. */
-/* #undef NO_MINUS_C_MINUS_O */
 
 /* Name of package */
 #define PACKAGE "rma-mt-benchmarks"
